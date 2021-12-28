f3b3c8d9eebc3b5a1a879571c8e2e24b8365c39b
johndavis <johndavis92790@gmail.com>
2021-12-27 21:58:16 -0700

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,18 @@
+//global variables
 var date = dayjs().format('M/D/YYYY')
 var currentDiv = document.querySelector('#cur-div');
 var dayDiv = document.querySelector('#five-day');
 var cityDiv = document.querySelector('#city-div');
 var cityInput = document.querySelector("#city-input");
 
+// to check if there are any cities saved in the localStorage
 function checkStorage() {
   if (localStorage.length > 0) {
     appendCities();
   }
 }
 
+// appends cities in localStorage to page as buttons, one at a time
 function appendCities() {
   var a = [];
   a = JSON.parse(localStorage.getItem('session')) || [];
@@ -18,14 +21,17 @@ function appendCities() {
   }
 }
 
+// appends a city from local storage as a button on page
 function appendCity(city){
   var cityId = city.split(' ').join('-');
   var appendCity = $('<button>').text(city).attr('id', cityId).addClass('col my-1 btn btn-secondary').attr('type', 'button mx-3').appendTo(cityDiv);
+  // creates event listener for each city
   $('#' + cityId).on("click", function () {
     checkCity(city);
   });
 }
 
+// checks if input is a valid city
 function checkCity(city){
   $(currentDiv).empty();
   $(dayDiv).empty();
@@ -34,6 +40,7 @@ function checkCity(city){
             cityFixed + '.json?' + 
             'access_token=pk.eyJ1Ijoiam9obmRhdmlzOTI3OTAiLCJhIjoiY2t4NDZlNWJ5MjVoYjJucW9kcm5jdGQxZyJ9.1CTxHYCYILbYZTJvrvtwqw')
     .then(function (res){
+      // creates location object
       var locationObj = {
         'lat' : res.data.features[0].center[1],
         'long' : res.data.features[0].center[0],
@@ -45,39 +52,50 @@ function checkCity(city){
     })
     .catch(function (err) {
       console.log(err);
+      // if city is not a valid city, it clears the input and asks the user to try again
       alert("Not a valid city, please try again!");
       cityInput.value = '';
   });
 }
 
+// to check if the city is already saved in the localStorage
 function checkStorageCity(city, locationObj) {
+  // if localStorage is not empty
   if (localStorage.length > 0) {
     var a = [];
     a = JSON.parse(localStorage.getItem('session')) || [];
     var search = false;
+    // checks each object in the localStorage array if it matches the city in the input field
     for(var i = 0 ; i < a.length; i++) {
       if (a[i] === city) {
+        // if it matches, it returns true
         search = true;
         break;
       } else {
+        // if not, returns false and tries the next object in the array
         search = false;
       }
     }
+    // if the input string matches any object in the localStorage
     if (search === true){
       getWeather(locationObj);
+    // if the input string does not match any object in the localStorage
     } else {
       appendCityLoc(city, locationObj);
     }
+  // if localStorage is empty 
   } else {
     appendCityLoc(city, locationObj);
   }
 }
 
+// appends a city from input as a button on page
 function appendCityLoc(city, locationObj){
   var cityId = city.split(' ').join('-');
   var appendCity = $('<button>').text(city).attr('id', cityId).addClass('col my-1 btn btn-secondary').attr('type', 'button mx-3').appendTo(cityDiv);
   saveCity(city);
   getWeather(locationObj);
+  // creates event listener for each city 
   $('#' + cityId).on("click", function () {
     checkCity(city);
   });
@@ -85,20 +103,22 @@ function appendCityLoc(city, locationObj){
 
 function saveCity(city){
   var a = [];
-  // Parse the serialized data back into an aray of objects
+  // create array from localStorage data
   a = JSON.parse(localStorage.getItem('session')) || [];
-  // Push the new data (whether it be an object or anything else) onto the array
+  // Push the new data (city name) onto the array
   a.push(city);
-  // Re-serialize the array back into a string and store it in localStorage
+  // Reset the array back into a string and store it in localStorage
   localStorage.setItem('session', JSON.stringify(a));
 }
 
+// gets weather data from API
 function getWeather(locationObj){
   axios.get('https://api.openweathermap.org/data/2.5/onecall?units=imperial&' + 
           'lat=' + locationObj.lat + 
           '&lon=' + locationObj.long + 
           '&appid=8ac56aa0cef179cc63eb58f2231b8356')
     .then(function (res){
+      // if successful, it creates object with all the needed info from the current day and the next 5 days
       var weatherObj = {
         '0' : {
           'date' : date,
@@ -149,6 +169,7 @@ function getWeather(locationObj){
       }
       appendData(weatherObj);
     })
+    // if the API is not working, it clears the input and asks the user to try again another time
     .catch(function (err) {
       console.log(err);
       alert("Cannot connect to weather data, please try again later");
@@ -156,8 +177,9 @@ function getWeather(locationObj){
   });
 }
 
+// creates elements and appends them to the DOM from the data within the weatherObj object
 function appendData(weatherObj) {
- 
+  // current day element
   var curDiv = $('<div>').addClass('col border border-secondary border-2 my-2').attr('style', 'height: 220px').appendTo(currentDiv);
   var curCity = $('<h4>').text(weatherObj.city_long + ' ' + dayjs().format('(M/D/YYYY)')).appendTo(curDiv);
   var curSpan = $('<span>').appendTo(curCity);
@@ -168,6 +190,7 @@ function appendData(weatherObj) {
   var curUV = $('<p>').text('UV Index: ').appendTo(curDiv);
   var curUVSpan = $('<span>').text(weatherObj[0].uv);
 
+  // checks uv rating to give it a background color depending the value, appends it to the current day element above
   if (weatherObj[0].uv < 3){
     curUVSpan.addClass('badge bg-success').appendTo(curUV);
   } else if (weatherObj[0].uv < 6){
@@ -176,34 +199,35 @@ function appendData(weatherObj) {
     curUVSpan.addClass('badge bg-danger').appendTo(curUV);
   }
 
+  // 1 day into the future weather element
   var oneDayDiv = $('<div>').addClass('col bg-info mx-2 border border-secondary').attr('style', 'height: 200px').appendTo(dayDiv);
   var oneDayDate = $('<h5>').text(dayjs().add(1, 'day').format('M/D/YYYY')).addClass('text-white pt-2').appendTo(oneDayDiv);
   var oneDayImg = $('<img>').attr('src', weatherObj[1].img).attr('alt', weatherObj[1].img_text).attr('style', 'height: 40px').appendTo(oneDayDiv);
   var oneDayTemp = $('<p>').text('Temp: ' + weatherObj[1].temp + ' °F').addClass('text-white').appendTo(oneDayDiv);
   var oneDayWind = $('<p>').text('Wind: ' + weatherObj[1].wind + ' MPH').addClass('text-white').appendTo(oneDayDiv);
   var oneDayHum = $('<p>').text('Humidity: ' + weatherObj[1].hum + '%').addClass('text-white').appendTo(oneDayDiv);
-  
+  // 2 days into the future weather element
   var twoDayDiv = $('<div>').addClass('col bg-info mx-2 border border-secondary').attr('style', 'height: 200px').appendTo(dayDiv);
   var twoDayDate = $('<h5>').text(dayjs().add(2, 'day').format('M/D/YYYY')).addClass('text-white pt-2').appendTo(twoDayDiv);
   var twoDayImg = $('<img>').attr('src', weatherObj[2].img).attr('alt', weatherObj[2].img_text).attr('style', 'height: 40px').appendTo(twoDayDiv);
   var twoDayTemp = $('<p>').text('Temp: ' + weatherObj[2].temp + ' °F').addClass('text-white').appendTo(twoDayDiv);
   var twoDayWind = $('<p>').text('Wind: ' + weatherObj[2].wind + ' MPH').addClass('text-white').appendTo(twoDayDiv);
   var twoDayHum = $('<p>').text('Humidity: ' + weatherObj[2].hum + '%').addClass('text-white').appendTo(twoDayDiv);
-
+  // 3 days into the future weather element
   var threeDayDiv = $('<div>').addClass('col bg-info mx-2 border border-secondary').attr('style', 'height: 200px').appendTo(dayDiv);
   var threeDayDate = $('<h5>').text(dayjs().add(3, 'day').format('M/D/YYYY')).addClass('text-white pt-2').appendTo(threeDayDiv);
   var threeDayImg = $('<img>').attr('src', weatherObj[3].img).attr('alt', weatherObj[3].img_text).attr('style', 'height: 40px').appendTo(threeDayDiv);
   var threeDayTemp = $('<p>').text('Temp: ' + weatherObj[3].temp + ' °F').addClass('text-white').appendTo(threeDayDiv);
   var threeDayWind = $('<p>').text('Wind: ' + weatherObj[3].wind + ' MPH').addClass('text-white').appendTo(threeDayDiv);
   var threeDayHum = $('<p>').text('Humidity: ' + weatherObj[3].hum + '%').addClass('text-white').appendTo(threeDayDiv);
-
+  // 4 days into the future weather element
   var fourDayDiv = $('<div>').addClass('col bg-info mx-2 border border-secondary').attr('style', 'height: 200px').appendTo(dayDiv);
   var fourDayDate = $('<h5>').text(dayjs().add(4, 'day').format('M/D/YYYY')).addClass('text-white pt-2').appendTo(fourDayDiv);
   var fourDayImg = $('<img>').attr('src', weatherObj[4].img).attr('alt', weatherObj[4].img_text).attr('style', 'height: 40px').appendTo(fourDayDiv);
   var fourDayTemp = $('<p>').text('Temp: ' + weatherObj[4].temp + ' °F').addClass('text-white').appendTo(fourDayDiv);
   var fourDayWind = $('<p>').text('Wind: ' + weatherObj[4].wind + ' MPH').addClass('text-white').appendTo(fourDayDiv);
   var fourDayHum = $('<p>').text('Humidity: ' + weatherObj[4].hum + '%').addClass('text-white').appendTo(fourDayDiv);
-
+  // 5 days into the future weather element
   var fiveDayDiv = $('<div>').addClass('col bg-info mx-2 border border-secondary').attr('style', 'height: 200px').appendTo(dayDiv);
   var fiveDayDate = $('<h5>').text(dayjs().add(5, 'day').format('M/D/YYYY')).addClass('text-white pt-2').appendTo(fiveDayDiv);
   var fiveDayImg = $('<img>').attr('src', weatherObj[5].img).attr('alt', weatherObj[5].img_text).attr('style', 'height: 40px').appendTo(fiveDayDiv);
@@ -212,10 +236,12 @@ function appendData(weatherObj) {
   var fiveDayHum = $('<p>').text('Humidity: ' + weatherObj[5].hum + '%').addClass('text-white').appendTo(fiveDayDiv);
 }
 
+// event listener for the search button
 document.querySelector("#search-button").addEventListener("click", function (event) {
   event.preventDefault();
   var cityInputTrim = cityInput.value.trim();
   checkCity(cityInputTrim);
 });
 
+// checks for cities stored in localStorage before anything
 checkStorage();
